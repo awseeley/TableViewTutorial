9bd0023e3c3146cf7caa83910bb10a488c65590e
Andrew Seeley <awseeley@gmail.com>
2014-10-05 19:22:50 +1100

    Sync with you tube tutorial
    Sync with you tube tutorial

--- TableViewTutorial/ViewController.swift
+++ TableViewTutorial/ViewController.swift
@@ -1,44 +1,50 @@
 
 
+
+
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var tableView: UITableView!
     
+    
     var tableData: [String] = ["Hello", "My", "Table"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         tableData.append("Seemu")
         self.tableView.reloadData()
         
-        println("test change")
     }
-    
-    // Table View Methods
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.tableData.count
     }
-
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell 	{
+     
         var cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
         cell.textLabel?.text = self.tableData[indexPath.row]
+        
         return cell
-    }    
-
-    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
-        println("Index \(indexPath.row) selected")
-    }
-
-    func reloadData() {
-        self.tableView.reloadData()
     }
     
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("Row \(indexPath.row) selected")
+    }
+
+
+    
+    
+    
+
+
     
 
 }
