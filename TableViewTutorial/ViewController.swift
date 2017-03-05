4afc006f343e7b5ade1a74136a3df796c3708063
Andrew Seeley <awseeley@gmail.com>
2017-03-05 20:08:48 +1100

    Search Tutorial Starter
    Search Tutorial Starter

--- TableViewTutorial/ViewController.swift
+++ TableViewTutorial/ViewController.swift
@@ -8,18 +8,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet var tableView: UITableView!
     
-    
-    var tableData: [String] = ["Hello", "My", "Table"]
+    var tableData: [String] = ["Red Velvet", "Brownie", "Bannna Bread", "Vanilla", "Minty"]
+    var tableDataDetail: [String] = ["Small", "Medium", "Large", "Small", "Medium"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
-        tableData.append("Seemu")
         self.tableView.reloadData()
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -27,10 +22,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell 	{
-     
-        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "cell")
         
         cell.textLabel?.text = self.tableData[indexPath.row]
+        cell.detailTextLabel?.text = self.tableDataDetail[indexPath.row]
         
         return cell
     }
@@ -38,14 +33,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("Row \(indexPath.row) selected")
     }
-
-
-    
-    
-    
-
-
-    
 
 }
 
